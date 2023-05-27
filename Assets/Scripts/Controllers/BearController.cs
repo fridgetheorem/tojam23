b5904f0766327da27bee79411c4860d2353a8f31
cameronhadfield <cameron.hadfield@gmail.com>
2023-05-27 14:07:44 -0400

    Added party controller, some animals

--- Assets/Scripts/Controllers/BearController.cs
+++ Assets/Scripts/Controllers/BearController.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class BearController : AnimalController
 {
     public float hitDamage = 1;
+    public float attackDistance = 1;
+    public float attackRadius = 1;
     // Start is called before the first frame update
-    void Start()
+    public override void Interact()
     {
-        
+        DoAttack();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void DoAttack(){
+# if DEBUG
+        Debug.Log("Attacking");
+# endif
+        // Do a sphere cast?
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, attackRadius, transform.forward, attackDistance);
+
+        // Damage each enemy 
+        foreach (RaycastHit hit in hits){
+# if DEBUG
+            Debug.Log("Hit", hit.collider.gameObject);
+# endif
+            IDamageable damageable = hit.collider.gameObject.GetComponent<IDamageable>();
+            damageable.BeDamaged(hitDamage);
+        }
     }
 }
--- Assets/Scripts/Controllers/LynxController.cs
+++ Assets/Scripts/Controllers/LynxController.cs
@@ -6,14 +6,29 @@ public class LynxController : AnimalController
 {
     public float screamRadius = 5;
     // Start is called before the first frame update
-    void Start()
+
+    public override void Interact()
     {
-        
+#if DEBUG
+        Debug.Log("Screech!");
+#endif
+
+        DoScream();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void DoScream(){
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, screamRadius);
+        if(hitColliders.Length == 0) return;
+
+        // Probably wanna play some sound
+
+        // We hit some collider
+        // Do something with this information
+        foreach ( Collider collider in hitColliders ){
+            if (!collider.GetComponent<CharacterController>()) return;
+            // Move all characters back to the edge of the circle
+        }
+
+
     }
 }
