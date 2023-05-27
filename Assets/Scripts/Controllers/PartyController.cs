f8bde787d8e3d059a15d91cd4dd515be84eb56cd
cameronhadfield <cameron.hadfield@gmail.com>
2023-05-27 13:01:57 -0400

    Push basic controls and movement

--- Assets/Scripts/Controllers/PartyController.cs
+++ Assets/Scripts/Controllers/PartyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimalController : MonoBehaviour
+public class PartyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
