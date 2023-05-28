851c7bc66ddd1e27c7e9654e96e74ac6f4e69846
jtan537 <jtan5372@gmail.com>
2023-05-28 02:17:23 -0400

    add: slow zone and win zone

---
+++ Assets/Scripts/Environment/SlowZone.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowZone : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _slowSpeed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Upon collision with another an animal object, this GameObject will be slowed
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Triggered");
+        if (other.gameObject.GetComponent<AnimalController>() != null)
+        {
+            other.gameObject.GetComponent<AnimalController>().speed = _slowSpeed;
+        }
+    }
+
+    //Upon collision with another an animal object, this GameObject will be slowed
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<AnimalController>() != null)
+        {
+            other.gameObject.GetComponent<AnimalController>().speed = other.gameObject.GetComponent<AnimalController>().originalSpeed;
+        }
+    }
+}
