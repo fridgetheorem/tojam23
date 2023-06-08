f80be98c93ef314cb640df8b87ba6f2b82b243e1
Zuhab Wasim <z-wasim-786@live.com>
2023-06-08 01:24:58 -0400

    Added audio saving

---
+++ Assets/Scripts/Core/DataManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+* Used for player configuration for
+*/
+
+public static class DataManager
+{
+    public static float audioBGM = 0.5f;
+    public static float audioSFX = 0.5f;
+    public static float audioMenu = 0.5f;
+}
