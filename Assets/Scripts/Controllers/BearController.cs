dd68408ff9365ea8ae23906fa9a88cbef2c6670a
Valerie Roussel <59619956+ValerieRoussel@users.noreply.github.com>
2023-06-15 22:00:03 -0400

    Bear can now attack. Enemies blink red when damaged and fade out when killed

--- Assets/Scripts/Controllers/BearController.cs
+++ Assets/Scripts/Controllers/BearController.cs
@@ -8,19 +8,58 @@ public class BearController : AnimalController
 {
     public float hitDamage = 1;
     public float attackDistance = 1;
-    public float attackRadius = 1;
+    public float attackRadius;
 
-    [SerializeField] GameObject attackVisual;
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject attackVisual;
+    
+    [SerializeField]
+    private float _attackCooldown = 1f;
+    private bool _canAttack = true;
+
     public override void Interact()
     {
+
+# if DEBUG
+        Debug.Log("Attacking");
+# endif
         DoAttack();
     }
 
     void DoAttack(){
-# if DEBUG
-        Debug.Log("Attacking");
-# endif
+
+        if (!_canAttack) return;
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRadius, LayerMask.GetMask("Enemy"));
+        if(hitColliders.Length > 0) {
+            // Probably wanna play some sound;
+
+            // We hit some collider
+            // Do something with this information
+            foreach ( Collider collider in hitColliders ){
+                // If the layer is Enemy, damage it
+                EnemyDamage ed = collider.gameObject.GetComponentInChildren<EnemyDamage>();
+                if (ed != null && ed.transform.parent.GetComponent<EnemyController>() != null) {
+                    ed.transform.parent.GetComponent<EnemyController>().BeDamaged(hitDamage);
+                }
+            }
+        }
+        _canAttack = false;
+        StartCoroutine(AttackCooldown(_attackCooldown));  
+    }
+
+    IEnumerator AttackCooldown(float length)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < length){
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        _canAttack = true;
+        yield return null;
+    }
+
+    void DoAttackOld() {
         // Do a sphere cast?
         RaycastHit[] hits = Physics.SphereCastAll(transform.position, attackRadius, transform.forward, attackDistance);
         
@@ -43,4 +82,5 @@ public class BearController : AnimalController
             animal?.BeDamaged(hitDamage);
         }
     }
+
 }
