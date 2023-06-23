c495260978eac43501489deb30811dc7a6c0090f
Zuhab Wasim <z-wasim-786@live.com>
2023-06-23 16:41:16 -0400

    Updated EnemyController to incorporate Animations

--- Assets/Scripts/Controllers/Enemy/EnemyController.cs
+++ Assets/Scripts/Controllers/Enemy/EnemyController.cs
@@ -9,7 +9,7 @@ public class EnemyController : AnimalController
 
     [Header("Enemy Properties")]
     [SerializeField]
-    private EnemyBehaviour behaviour = EnemyBehaviour.Aggressive;
+    private EnemyBehaviour behaviour = EnemyBehaviour.Passive;
 
     [SerializeField]
     private float aggroRange = 5f;
@@ -49,7 +49,19 @@ public class EnemyController : AnimalController
         PartyController party;
         if (!(party = collider.gameObject.GetComponentInChildren<PartyController>()))
             return;
+
+        behaviour = EnemyBehaviour.Aggressive;
         TrackPlayerRoutine(party);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        AnimalController animal = other.gameObject.GetComponent<AnimalController>();
+        if (animal != null)
+        {
+            behaviour = EnemyBehaviour.Passive;
+            UpdateAnimator("Moving", false);
+        }
     }
 
     void TrackPlayerRoutine(PartyController party)
@@ -59,9 +71,8 @@ public class EnemyController : AnimalController
             case EnemyBehaviour.Aggressive:
                 Vector3 movementVec = party.transform.position - transform.position;
                 if (!ed.IsHurting())
-                {
                     Move(movementVec, speed);
-                }
+                AnimateEnemy(new Vector2(movementVec.x, movementVec.z));
                 break;
             case EnemyBehaviour.Passive:
                 // Just do nothing
@@ -69,6 +80,20 @@ public class EnemyController : AnimalController
             default:
                 break;
         }
+    }
+
+    void AnimateEnemy(Vector2 movement2D)
+    {
+        Animate(movement2D);
+
+        // The value 1.35 is the distance where the enemy starts hugging the player (no need to make them move then).
+        UpdateAnimator(
+            "Moving",
+            movement2D.magnitude > 1.35f && behaviour == EnemyBehaviour.Aggressive
+        );
+        FlipSprite(movement2D.x < 0);
+        UpdateAnimator("Down", movement2D.y >= 0);
+        // PlayFootsteps(movement2D); TODO: Enemy local audio footsteps
     }
 
     public void DamageBehaviour(float garbage1, float garbage2)
